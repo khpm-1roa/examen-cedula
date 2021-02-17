e43da553058422209b2fb0ce5bc2294d14817692
PILLAJO-HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-02-16 21:40:03 -0500

    EXAMEN

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require ("express");
+const app = express ();
+const url = require('url');
+const path = require ('path');
+const port = 3000;
+
+app.get('/fibonacci',(req, res) => {
+    fibonacci = [];
+    let numero = parseFloat(req.query.numero);
+     parImpar = (numero) => {
+        if(numero % 2 === 0) {
+          return "par";
+        }
+        else {
+          return "impar";
+        }
+      }
+    if ( parImpar(numero) === "par" ) {
+        let numero1= 0,
+        numero2= 1,
+        resultado = numero1+numero2;
+    for (let i=0; i< 20 ;i++){
+        resultado = numero1 + numero2;
+        numero1 = numero2;
+        numero2 = resultado;
+
+        fibonacci.push(resultado);
+    }
+    }else
+    {
+        let numero1= 0,
+        numero2= 1,
+        resultado = numero1+numero2;
+    for (let i=0; i< 10 ;i++){
+        resultado = numero1 + numero2;
+        numero1 = numero2;
+        numero2 = resultado;
+        fibonacci.push(resultado);
+    }
+    }
+    res.json(`La respuesta es : ${fibonacci}`);
+})
+
+app.listen(port, ()=> {
+    console.log(`Escucha de el puerto 3000 ${port}!`)
+}
+)
